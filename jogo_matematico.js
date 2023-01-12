fc2a96e3652fd290e90c71ffcd8f3c573be0f562
Samara Mariah <samaramariah16@gmail.com>
2023-01-12 13:04:25 -0300

    update jogo matematico

--- jogo_matematico.js
+++ jogo_matematico.js
@@ -1,24 +1,31 @@
-//gerando números aleatórios, o floor(piso) arrendonda pra baixo, e abs é absoluto
-
 let acumuladorDePontos = 0;
-//como adicionar pontos ao acumulador de pontos
-acumuladorDePontos = acumuladorDePontos + 5;
+let acumuladordevidas = 3;
 
-//1. Adicionar 5 pontos sempre que a pergunta for correta
-//2. Mostrar quantos pontos ele fez no final
-//3. Se o valor for correto, não precisa mostrar que ele está correto, so precisa fazer uma nova pergunta
+// como adicionar pontos ao acumulador de pontos
+// acumuladorDePontos = acumuladorDePontos + 5;
 
-while(true){
-    let numero1 = Math.floor(Math.abs(Math.random() * (100 - 0) + 0));
-    let numero2 = Math.floor(Math.abs(Math.random() * (100 - 0) + 0));
+// 1. Adicionar 5 pontos sempre que a pergunta for correta
+// 2. Mostrar quantos pontos ele fez no final
+// 3. Se o valor for correto não precisa mostrar que o valor está correto so
+// .  precisa fazer uma nova pergunta (PS. não deixem o if vazio)
 
-    let valorDigitado = prompt("Quanto é " +numero1+ "+" +numero2+"?")
 
-   if(valorDigitado != numero1 + numero2){
-    alert("Incorreto, você fez (x) pontos");
-    break;
-   }
+while (true) {
+  let numero1 = Math.floor(Math.abs(Math.random() * (100 - 0) + 0));  //gerando números aleatórios, o floor(piso) arrendonda pra baixo, e abs é absoluto
+  let numero2 = Math.floor(Math.abs(Math.random() * (100 - 0) + 0));
 
-console.log(numero1 +"\n" + numero2);
+  let valorDigitado = prompt("Quanto é " + numero1 + "+" + numero2 + "?");
+
+  if (valorDigitado == numero1  +  numero2) {
+    acumuladorDePontos += 5;
+  } else {
+    acumuladordevidas -= 1;
+    
+    if (acumuladordevidas == 0 ) {
+      alert("Incorreto, Suas vidas acabaram. Você atingiu " + acumuladorDePontos +  " pontos");
+      break;
+    } else {
+      alert("Incorreto, você tem " + acumuladordevidas + " vidas");
+    }
+  }
 }
-
